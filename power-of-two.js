7f47bd160306ab97c96ae6458808c871aec9029a
Rohan Kumar <rohankhg18@gmail.com>
2024-06-26 22:11:54 +0530

    power of 2

---
+++ power-of-two.js
@@ -0,0 +1,35 @@
+
+//Method 1
+function powerOfTwo(num) {
+    let isPower = true;
+    for (let i = num; i >= 2; i = i / 2) {
+        if (i % 2 === 0) {
+            isPower = true;
+        }
+        else {
+            return false;
+        }
+    }
+    return isPower
+}
+
+console.log(powerOfTwo(3))
+
+//Big-O = log(n)
+
+
+//Method 2
+function isPowerOfTwo(n) {
+    if (n < 1) {
+        return false;
+    }
+    while (n > 1) {
+        if (n % 2 !== 0) {
+            return false;
+        }
+        n = n / 2;
+    }
+    return true;
+}
+
+//Big-O = log(n)
